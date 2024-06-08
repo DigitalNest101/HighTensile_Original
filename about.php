604baf1314aacf42797f905387c682f9df54f9dc
DigitalNest101 <84118920+DigitalNest101@users.noreply.github.com>
2024-06-08 12:57:15 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,76 @@
+<?php include('./assets/partials/header.php')  ?>
+
+
+  <main id="main" style="margin-top: 106px;">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs d-flex align-items-center" style="background-image: url('assets/images/2.jpg');">
+      <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
+
+        <h2>About</h2>
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>About</li>
+        </ol>
+
+      </div>
+    </div>
+    <!-- End Breadcrumbs -->
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row position-relative">
+          <div class="col-lg-7 about-img" style="background-image: url(assets/images/abourt.jpg);"></div>
+
+          <div class="col-lg-7">
+            <h2>HIGH TENSILE</h2>
+            <div class="our-story">
+              <h4>Est 1988</h4>
+              <h3>Our Story</h3>
+             <p>
+             High Tencile offers premium CNC lazer cutting services for a variety of materials, including sheet metal fabrications made from sheets of ALUMINUM, CRC, GI, and SS. The most recent technology and a well-equipped infrastructure for manufacture of any kind. PRESSED STEEL DOORS, WINDOW FRAMES, FIRE BOXES, ELECTRONIC & ELECTRICAL ENCLOSURE.
+
+
+             The company has hired technical experts to make sure that appropriate measures have been implemented to preserve quality at all manufacturing stages and to ensure that items are produced perfectly for the complete delight of the customers.
+These industrial CNC & Bending services are highly regarded in the market for their qualities including prompt product implementation, reasonable and inexpensive prices, prompt finished delivery, and dependability.
+
+             </p>
+            
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    <!-- End About Section -->
+
+   
+
+    <!-- ======= Alt Services Section ======= -->
+    <section id="alt-services" class="alt-services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row justify-content-around gy-4">
+          <div class="col-lg-6 img-bg" style="background-image: url(assets/images/about4.jpg) ; background-position: center; background-size: cover;" data-aos="zoom-in" data-aos-delay="100"></div>
+
+          <div class="col-lg-5 d-flex flex-column justify-content-center">
+            <h2>WHAT WE PROVIDE</h2>
+           <P>
+           The company guarantees that there will be no compromises made in the quality of the raw materials used, for joints, or for the final paint finish of the products, and offers the full product line to the customers at extremely competitive and reasonable pricing. As a result, the products are accepted first. For diverse operations including straightening, cutting, moulding, and shaping metal sheets as well as for other varied processes in stages of manufacturing, assembling, and finishing, various sections have been built up.
+
+           </P>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Alt Services Section -->
+
+  
+        
+
+  </main><!-- End #main -->
+
+<?php include('./assets/partials/footer.php')  ?>
---
+++ index.php
@@ -0,0 +1,189 @@
+<?php include('./assets/partials/header.php')  ?>
+<!-- @@include('./partials/header.php') -->
+
+	<main class="site-content" role="main">
+		<!--
+        Home Slider
+        ==================================== -->
+
+		<section id="home-slider" class="aaaa">
+			<div id="slider" class="sl-slider-wrapper">
+
+				<div class="sl-slider">
+					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25"
+						data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+						<div class="mask-overly"></div>
+						<div class="bg-img slider-1">
+							<div class="slide-caption">
+								<div class="caption-content">
+									<h2 class="animated fadeInDown">
+										HIGH TENSILE 
+									</h2>
+									<!-- <p class="animated fadeInDown">Duis autem vel eum iriure dolor in hendrerit in
+										vulputate velit esse molestie
+										consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan
+										et iusto odio dignissim qui
+										blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla
+										facilisi.</p> -->
+									<!-- <a href="#" class="btn btn-blue btn-effect">Join US</a> -->
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+					<div class="sl-slide" data-orientation="vertical" data-slice1-rotation="10"
+						data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
+						<div class="mask-overly"></div>
+						<div class="bg-img slider-2"></div>
+
+						<div class="slide-caption">
+							<div class="caption-content">
+								<h2 class="animated fadeInDown">HIGH TENCILE</h2>
+								<!-- <p class="animated fadeInDown">Duis autem vel eum iriure dolor in hendrerit in vulputate
+									velit esse molestie
+									consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et
+									iusto odio dignissim qui
+									blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+								</p> -->
+								<!-- <a href="#" class="btn btn-blue btn-effect">Join US</a> -->
+							</div>
+						</div>
+
+					</div>
+
+					<div class="sl-slide " data-orientation="horizontal" data-slice1-rotation="3"
+						data-slice2-rotation="3" data-slice1-scale="2" data-slice2-scale="1">
+						<div class="mask-overly"></div>
+						<div class="bg-img slider-3"></div>
+						<div class="slide-caption">
+							<div class="caption-content">
+								<h2 class="animated fadeInDown">HIGH TENCILE</h2>
+								<!-- <p class="animated fadeInDown">
+									Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie
+									consequat, vel illum dolore eu
+									feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit
+									praesent luptatum zzril
+									delenit augue duis dolore te feugait nulla facilisi.
+								</p> -->
+								<!-- <a href="#" class="btn btn-blue btn-effect">Join US</a> -->
+							</div>
+						</div>
+
+					</div>
+
+				</div><!-- /sl-slider -->
+
+				<nav id="nav-arrows" class=" nav-arrows hidden-xs hidden-sm visible-md visible-lg">
+					<a href="javascript:;" class="sl-prev">
+						<i class="fa fa-angle-left fa-3x"></i>
+					</a>
+					<a href="javascript:;" class="sl-next">
+					<i class="fa fa-angle-right fa-3x"></i>
+						
+					</a>
+				</nav>
+
+
+				<nav id="nav-dots" class="nav-dots visible-xs visible-sm hidden-md hidden-lg">
+					<span class="nav-dot-current"></span>
+					<span></span>
+					<span></span>
+				</nav>
+
+			</div><!-- /slider-wrapper -->
+		</section>
+
+		<!--
+        End Home SliderEnd
+        ==================================== -->
+
+		<div class="container bootstrap snippets bootdeys ">
+			<div class="row">
+				<div class="col-md-12 col-sm-12 content-card ">
+					<div class="card-big-shadow">
+						<div class="card card-just-text" data-background="color" data-color="card" data-radius="none">
+							<div class="content ">
+								<h6 class="category">Best cards</h6>
+								<h4 class="title"><a class="" href="#">WELCOME TO HIGH TENSILE FASTNERS AND TOOLS</a></h4>
+								<p class="description ">Our firm was incorporated in the year 2009 as a Partnership entity at Balanagar in
+									Hyderabad. The partner, Mr. Parashuram has excellent entrepreneur skills of running
+									the
+									business. He has remained totally devoted towards all the business activities right
+									from
+									purchase of Raw Materials to fabrication of products and to final finish till
+									delivery.
+									The product range Sheet metal fabrications using the ALUMINUM, CRC, GI, SS SHEETS.
+									latest technology with well equipped infrastructure for any type of manufacturing.
+									ELECTRONIC & ELECTRICAL ENCLOSURE, FIRE BOXES, PRESSED STEEL DOORS WINDOW FRAMES.
+									Factory Establishd in The 2009-made under strict supervision in our fully equipped
+									industrial unit situated at Hydarabad.. The firm has employed technical
+									professionals to
+									ensure effective steps have taken to maintain the quality at all stages of
+									production
+									and to take care of perfection of products fabricated for the full satisfaction of
+									the
+									clients. The competence of our personnel is backed up by many years of experience in
+									the
+									profession.. </p>
+							</div>
+						</div> <!-- end card -->
+					</div>
+				</div>
+			</div>
+			<section id="feature">
+				<div class="container">
+					<div class="row">
+						<!-- <div class="col-lg-12">
+							<div class="section-title text-center">
+								<h2>Welcome to High Tensile Fastners and Tools</h2>
+								<p>Our firm was incorporated in the year 2009 as a Partnership entity at Balanagar in
+									Hyderabad. The partner, Mr. Parashuram has excellent entrepreneur skills of running
+									the
+									business. He has remained totally devoted towards all the business activities right
+									from
+									purchase of Raw Materials to fabrication of products and to final finish till
+									delivery.
+									The product range Sheet metal fabrications using the ALUMINUM, CRC, GI, SS SHEETS.
+									latest technology with well equipped infrastructure for any type of manufacturing.
+									ELECTRONIC & ELECTRICAL ENCLOSURE, FIRE BOXES, PRESSED STEEL DOORS WINDOW FRAMES.
+									Factory Establishd in The 2009-made under strict supervision in our fully equipped
+									industrial unit situated at Hydarabad.. The firm has employed technical
+									professionals to
+									ensure effective steps have taken to maintain the quality at all stages of
+									production
+									and to take care of perfection of products fabricated for the full satisfaction of
+									the
+									clients. The competence of our personnel is backed up by many years of experience in
+									the
+									profession..</p>
+							</div>
+						</div> -->
+						<div class="col-md-4 p-5">
+							<h4>The Perfect Spot</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quia praesentium culpa a!
+								Est
+								fuga, blanditiis
+								rem culpa sed unde.</p>
+							<ul class="list-nav">
+								<li><i class="ion-ios-checkmark-empty"></i>Norman Copenhagen focuses on challenging</li>
+								<li><i class="ion-ios-checkmark-empty"></i>The challenge was melting</li>
+								<li><i class="ion-ios-checkmark-empty"></i>We thought air purifier</li>
+								<li><i class="ion-ios-checkmark-empty"></i>Front face is coated with fabric for a
+									familia
+								</li>
+								<li><i class="ion-ios-checkmark-empty"></i>Developing Belyse, I thought the strong
+									characters</li>
+							</ul>
+						</div>
+						<div class="col-md-8">
+							<img class="img-fluid" src="assets/images/features2.jpg" alt="error">
+						</div>
+					</div>
+				</div>
+			</section>
+
+	</main>
+
+<?php include('./assets/partials/footer.php') ?>
+<!-- @@include('./partials/footer.php') -->
